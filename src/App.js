b0bf957f9cadf6e869ed62484d09857180137e88
eunji <lej970703@naver.com>
2021-01-03 00:56:20 +0900

    make Movie component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import axios from 'axios';
+import Movie from './Movie';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    isLoading : true,
+    movies : [],
+  };
+  getMovies = async () => {
+    const {
+      data : {
+        data : { movies },
+      },
+    } = await axios.get('https://yts-proxy.now.sh/list_movies.json?sort_by=rating');
+    this.setState({movies, isLoading : false});
+  }
+  //컴포넌트가 마운트되면 axios.get()함수가 실행되며 영화 데이터를 가져온다.
+  componentDidMount(){
+    //영화 데이터 로딩!!
+    //axios.get("https://yts-proxy.now.sh/list_movies.json");
+    this.getMovies();
+  }
+  render(){
+    const {isLoading, movies} = this.state;
+    return <div> 
+      {isLoading 
+        ? 'Loading....' 
+        : movies.map((movie)=>{
+        return <Movie 
+                key={movie.id}
+                id={movie.id}
+                year={movie.year}
+                title={movie.summary}
+                poster={movie.medium_cover_image}
+        />;
+      })} 
+      </div>;
+  }
 }
 
 export default App;
